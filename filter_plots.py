3e3bcd86ffdc668a2996d534d043d0341a83ff9c
Matthew Farrell <matthew.farrell.235@gmail.com>
2021-08-30 09:53:49 -0400

    added reduced filter plots.

--- filter_plots.py
+++ filter_plots.py
@@ -9,13 +9,12 @@ C[1,1]=.3
 C[-1,-1]=.3
 Phis = Phi.conj().T
 A = (Phi @ C @ Phis).real
-Phir = np.array([[],[]])
-Phir = scipy.linalg.dft(2)
+Phir = np.array([[1, 1j],[1j, 1]])
 Phirs = Phir.conj().T
-Cr = np.zeros((2,2))
-Cr[0,0] = Cs[1,1]
-Cr[1,1] = Cs[-1,-1]
-Ar = (Phir @ Cr @ Phirs).real
+Cr = np.zeros((2,n))
+Cr[1,0] = C[1,1]
+Cr[-1,1] = C[-1,-1]
+Ar = (Phir @ Cr @ Phis).real
 
 fig, ax = plt.subplots()
 ax.imshow(A)
@@ -23,18 +22,35 @@ ax.axes.get_xaxis().set_visible(False)
 ax.axes.get_yaxis().set_visible(False)
 fig.savefig('first_mode_filters.pdf')
 fig.show()
+fig, ax = plt.subplots()
+ax.imshow(Ar)
+ax.axes.get_xaxis().set_visible(False)
+ax.axes.get_yaxis().set_visible(False)
+fig.savefig('first_mode_reduced_filters.pdf')
+fig.show()
 # fig, ax = plt.subplots()
 # ax.imshow(Ar)
 # fig.show()
 
 C[2,2]=.3
 C[-2,-2]=.3
+Cr[2,0] = C[1,1]
+Cr[-2,1] = C[-1,-1]
 A = (Phi @ C @ Phis).real
+Ar = (Phir @ Cr @ Phis).real
+
 fig, ax = plt.subplots()
 ax.imshow(A)
 ax.axes.get_xaxis().set_visible(False)
 ax.axes.get_yaxis().set_visible(False)
 fig.savefig('first_and_second_mode_filters.pdf')
+fig.show()
+
+fig, ax = plt.subplots()
+ax.imshow(Ar)
+ax.axes.get_xaxis().set_visible(False)
+ax.axes.get_yaxis().set_visible(False)
+fig.savefig('first_and_second_mode_reduced_filters.pdf')
 fig.show()
 
 r1 = np.zeros(n)
