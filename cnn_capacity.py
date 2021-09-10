25635fc5f5b7d3860db90d17833c822acc174456
Matthew Farrell <msfarrell@seas.harvard.edu>
2021-09-10 15:15:32 -0400

    synchronizing

--- cnn_capacity.py
+++ cnn_capacity.py
@@ -1,23 +1,64 @@
 import timm
+from models import gridcellconv
 import torch
+import itertools
 from torchvision.transforms import ToTensor
 
 T = ToTensor()
 
+def get_shifted_img(img: torch.Tensor, gx: int, gy: int):
+    img_ret = img.clone()
+    img_ret = torch.roll(img_ret, (gx, gy), dims=(-2, -1))
+
 effnet = timm.models.factory.create_model('efficientnet_b2', pretrained=True)
 
-dataset = timm.data.dataset_factory.create_dataset(
+core_dataset = timm.data.dataset_factory.create_dataset(
     'ImageFolder',
     root='/home/matthew/datasets/imagenet/ILSVRC/Data/CLS-LOC',
     batch_size=1)
 
+class ShiftDataset(torch.utils.data.Dataset):
+    def __init__(self, core_dataset, ):
+        self.core_dataset = core_dataset
+        img_s = self.core_dataset[0][0].shape
+        self.sx = img_s[-2]
+        self.sy = img_s[-1]
+        # self.G = itertools.product((range(self.xs), range(sy))) # Lazy approach
+
+    def __len__(self):
+        return len(self.core_dataset)*self.sx*self.sy
+
+    def __getitem__(self, idx):
+        g_idx = idx % (self.sx*self.sy)
+        idx_core = idx // (self.sx*self.sy)
+        img = self.core_dataset[idx_core]
+        # g = self.G[g_idx] # Lazy approach
+        gx = g_idx // self.sy 
+        gy = g_idx % self.sy
+        img, label = self.core_dataset[idx_core]
+        return get_shifted_img(img, gx, gy), label
+
+
+dataset = ShiftDataset(core_dataset)
 dataloader = timm.data.loader.create_loader(dataset, (3,224,224), 1)
 
 img = next(iter(dataloader))[0]
+# %% 
 
-def get_shifted_img(img: torch.Tensor, gx: int, gy: int):
-    img_ret = img.clone()
-    img_ret = torch.roll(img_ret, (gx, gy), dims=(-2, -1))
+class Perceptron(torch.nn.Module):
+    def __init__(self, N):
+        self.readout_w = torch.nn.Parameter(torch.randn(N) / N**.5)
+
+    def forward(self, input):
+        return input @ self.readout_w
+
+net = Perceptron(20)
+
+epochs = 5
+for epoch in range(epochs):
+    for x, y in 
+
+
 
 # batch = torch.stack([T(dataset_train[k][0]) forkk in range(2)])
 # %% 
