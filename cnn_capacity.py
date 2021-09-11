8ed14c95ef3de2dcd2d4b2dc08ff468d96846b94
Matthew Farrell <matthew.farrell.235@gmail.com>
2021-09-10 22:35:12 -0400

    Continuing to work on cnn capacity experiments.
    The code now generates shifted version of ImageNet images and produces
    random labels to use to train a Perceptron. Hasn't been tested much.

--- cnn_capacity.py
+++ cnn_capacity.py
@@ -1,28 +1,50 @@
 import timm
 from models import gridcellconv
 import torch
+import torchvision
 import itertools
-from torchvision.transforms import ToTensor
+import torchvision.transforms as transforms
+# from matplotlib import pyplot as plt
 
-T = ToTensor()
+n_dichotomies = 100
+n_inputs = 1000
+epochs = 5
+batch_size = 124
+
+# image_net_dir = '/home/matthew/datasets/imagenet/ILSVRC/Data/CLS-LOC/val'
+image_net_dir = '/n/pehlevan_lab/Lab/matthew/imagenet/ILSVRC/Data/CLS-LOC/val'
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
+transform_train = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    # normalize,
+])
+transform_test = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    # normalize,
+])
+def random_label(label):
+    if torch.rand(1).item() < .5:
+        return -1
+    else:
+        return 1
 
 def get_shifted_img(img: torch.Tensor, gx: int, gy: int):
     img_ret = img.clone()
     img_ret = torch.roll(img_ret, (gx, gy), dims=(-2, -1))
-
-effnet = timm.models.factory.create_model('efficientnet_b2', pretrained=True)
-
-core_dataset = timm.data.dataset_factory.create_dataset(
-    'ImageFolder',
-    root='/home/matthew/datasets/imagenet/ILSVRC/Data/CLS-LOC',
-    batch_size=1)
+    return img_ret
 
 class ShiftDataset(torch.utils.data.Dataset):
-    def __init__(self, core_dataset, ):
+    def __init__(self, core_dataset):
         self.core_dataset = core_dataset
-        img_s = self.core_dataset[0][0].shape
-        self.sx = img_s[-2]
-        self.sy = img_s[-1]
+        self.sx, self.sy = self.core_dataset[0][0].shape[1:]
+        self.targets = torch.tile(torch.tensor(self.core_dataset.targets),
+                                  (self.sx*self.sy,))
         # self.G = itertools.product((range(self.xs), range(sy))) # Lazy approach
 
     def __len__(self):
@@ -31,64 +53,95 @@ class ShiftDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         g_idx = idx % (self.sx*self.sy)
         idx_core = idx // (self.sx*self.sy)
-        img = self.core_dataset[idx_core]
         # g = self.G[g_idx] # Lazy approach
         gx = g_idx // self.sy 
         gy = g_idx % self.sy
         img, label = self.core_dataset[idx_core]
         return get_shifted_img(img, gx, gy), label
 
+class HingeLoss(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, output, target):
+
+        hinge_loss = 1 - torch.mul(output, target)
+        hinge_loss[hinge_loss < 0] = 0
+        return hinge_loss.mean()
+
+
+effnet = timm.models.factory.create_model('efficientnet_b2', pretrained=True)
+core_dataset = torchvision.datasets.ImageFolder(root=image_net_dir,
+                                                transform=transform_train)
+class RandomLabels:
+    def __init__(self, batch_size, seed):
+        self.seed = seed
+        torch.manual_seed(self.seed)
+        self.batch_size = batch_size
+
+    def reset(self):
+        torch.manual_seed(self.seed)
+    
+    def get_random_labels(self):
+        return 2*(torch.rand(self.batch_size) < 0.5) - 1
+
+# core_dataset = timm.data.dataset_factory.create_dataset(
+    # 'ImageFolder',
+    # root='/home/matthew/datasets/imagenet/ILSVRC/Data/CLS-LOC',
+    # batch_size=1)
+
+                                         # sampler=binary_sampler)
+# dataloader = timm.data.loader.create_loader(dataset, (3,224,224), 1,
+                                           # num_workers=0,
+                                            # persistent_workers=False,)
+                                           # # sampler=binary_sampler)
 
 dataset = ShiftDataset(core_dataset)
-dataloader = timm.data.loader.create_loader(dataset, (3,224,224), 1)
-
-img = next(iter(dataloader))[0]
+sub_sampler = torch.utils.data.sampler.SubsetRandomSampler(
+    range(n_inputs))
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                         num_workers=0,
+                                        sampler=sub_sampler)
+random_labels = RandomLabels(batch_size, 2)
+test_input, test_label = next(iter(dataloader))
+# plt.figure(); plt.imshow(dataset[100][0].transpose(0,2).transpose(0,1)); plt.show()
 # %% 
+# loss_fn = torch.nn.HingeEmbeddingLoss()
+loss_fn = HingeLoss()
 
-class Perceptron(torch.nn.Module):
-    def __init__(self, N):
-        self.readout_w = torch.nn.Parameter(torch.randn(N) / N**.5)
+h_test = effnet.get_features(test_input)
+h0 = h_test[0]
+N = torch.prod(torch.tensor(h0.shape[1:]))
 
-    def forward(self, input):
-        return input @ self.readout_w
+for k1 in range(n_dichotomies):
+    # dataset = ShiftDataset(core_dataset)
+    # binary_sampler = torch.utils.data.sampler.SubsetRandomSampler(binary_indices)
+    # dataloader = torch.utils.data.DataLoader(dataset, batch_size=124,
+                                             # num_workers=0,
+                                            # sampler=sub_sampler)
+    class Perceptron(torch.nn.Module):
+        def __init__(self, N):
+            super().__init__()
+            self.readout_w = torch.nn.Parameter(torch.randn(N) / N**.5)
 
-net = Perceptron(20)
+        def forward(self, input):
+            return input @ self.readout_w
 
-epochs = 5
-for epoch in range(epochs):
-    for x, y in 
+    perceptron = Perceptron(N)
 
-
-
-# batch = torch.stack([T(dataset_train[k][0]) forkk in range(2)])
-# %% 
-effnet.to('cuda')
-h = effnet.get_features(img.to('cuda'))
-
-def get_features_shifted(feature_function, dataloader, num_samples):
-    img = next(iter(dataloader))[0]
-    batch_size, channels, sx, sy = img.shape
-    features = feature_function(img)
-    features_full = [torch.zeros(num_samples, sx, sy, *feature.shape[1:])
-                    for feature in features]
-    features = []
-    cnt = 0
-    for img, label in dataloader:
-        for gx in range(sx):
-            for gy in range(sy):
-                img_shifted = get_shifted_img(img, gx, gy)
-                features_shifted = feature_function(img_shifted)
-
-    
-
-# pretrain_batchsize = 256
-# dataset_pretrain = timm.data.dataset_factory.create_dataset(
-    # 'ImageFolder',
-    # root='/n/pehlevan_lab/Lab/matthew/imagenet/ILSVRC/Data/CLS-LOC',
-    # split=args.train_split, is_training=True,
-    # batch_size=pretrain_batchsize)
-# dataset_eval = timm.data.dataset_factory.create_dataset(
-    # 'ImageFolder',
-    # root='/n/pehlevan_lab/Lab/matthew/imagenet/ILSVRC/Data/CLS-LOC',
-    # split=args.val_split, is_training=False, batch_size=args.batch_size)
+    optimizer = torch.optim.Adam(perceptron.parameters(), lr=.0001)
+    for epoch in range(epochs):
+        random_labels.reset()
+        for input, label in dataloader:
+            random_label_batch = random_labels.get_random_labels()
+            h = effnet.get_features(input)[0]
+            h = h.reshape(h.shape[0], -1)
+            optimizer.zero_grad()
+            out = perceptron(h)
+            # y = 2*label - 1
+            loss = loss_fn(out, random_label_batch)
+            loss.backward()
+            optimizer.step()
+            print(loss.item())
 
