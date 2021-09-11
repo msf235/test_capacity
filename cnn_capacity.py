2b66a36df46ad4a00b753e8ea54c6e8f0d8bf27c
Matthew Farrell <matthew.farrell.235@gmail.com>
2021-09-11 10:48:57 -0400

    Cleaned up the code a bit.

--- cnn_capacity.py
+++ cnn_capacity.py
@@ -44,7 +44,7 @@ class ShiftDataset(torch.utils.data.Dataset):
         self.core_dataset = core_dataset
         self.sx, self.sy = self.core_dataset[0][0].shape[1:]
         # self.targets = torch.tile(torch.tensor(self.core_dataset.targets),
-                                  # (self.sx*self.sy,))
+                                  # (self.sx*self.sy,))  # Too large for memory
         # self.G = itertools.product((range(self.xs), range(sy))) # Lazy approach
 
     def __len__(self):
@@ -60,12 +60,10 @@ class ShiftDataset(torch.utils.data.Dataset):
         return get_shifted_img(img, gx, gy), label
 
 class HingeLoss(torch.nn.Module):
-
     def __init__(self):
         super().__init__()
 
     def forward(self, output, target):
-
         hinge_loss = 1 - torch.mul(output, target)
         hinge_loss[hinge_loss < 0] = 0
         return hinge_loss.mean()
@@ -98,21 +96,26 @@ class RandomLabels:
                                            # # sampler=binary_sampler)
 
 dataset = ShiftDataset(core_dataset)
-sub_sampler = torch.utils.data.sampler.SubsetRandomSampler(
-    range(n_inputs))
+sub_sampler = torch.utils.data.sampler.SubsetRandomSampler( range(n_inputs))
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                         num_workers=0,
-                                        sampler=sub_sampler)
+                                         num_workers=0, sampler=sub_sampler)
 random_labels = RandomLabels(batch_size, 2)
 test_input, test_label = next(iter(dataloader))
 # plt.figure(); plt.imshow(dataset[100][0].transpose(0,2).transpose(0,1)); plt.show()
 # %% 
-# loss_fn = torch.nn.HingeEmbeddingLoss()
 loss_fn = HingeLoss()
 
 h_test = effnet.get_features(test_input)
 h0 = h_test[0]
 N = torch.prod(torch.tensor(h0.shape[1:]))
+
+class Perceptron(torch.nn.Module):
+    def __init__(self, N):
+        super().__init__()
+        self.readout_w = torch.nn.Parameter(torch.randn(N) / N**.5)
+
+    def forward(self, input):
+        return input @ self.readout_w
 
 for k1 in range(n_dichotomies):
     # dataset = ShiftDataset(core_dataset)
@@ -120,13 +123,6 @@ for k1 in range(n_dichotomies):
     # dataloader = torch.utils.data.DataLoader(dataset, batch_size=124,
                                              # num_workers=0,
                                             # sampler=sub_sampler)
-    class Perceptron(torch.nn.Module):
-        def __init__(self, N):
-            super().__init__()
-            self.readout_w = torch.nn.Parameter(torch.randn(N) / N**.5)
-
-        def forward(self, input):
-            return input @ self.readout_w
 
     perceptron = Perceptron(N)
 
