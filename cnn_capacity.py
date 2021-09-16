589133d76a9e9537c9bb54b599e1e597227fe499
Matthew Farrell <msfarrell@seas.harvard.edu>
2021-09-16 13:07:58 -0400

    Variable sized shifts and outer parallelization.
    Added the option to use variable sized shifts, so
    that capacity of subgroups can be investigated.
    Not very well tested yet.
    
    Also added the option for running code in parallel
    over the inner loop (# dichotomies) or the outer
    loop (# inputs / # channels).

--- cnn_capacity.py
+++ cnn_capacity.py
@@ -20,28 +20,38 @@ import model_output_manager as mom
 
 output_dir = 'output'
 fig_dir = 'figs'
-# rerun = True # If True, rerun the simulation even if a matching simulation is
+rerun = True # If True, rerun the simulation even if a matching simulation is
                # found saved to disk
-rerun = False
-n_cores = 10 # Number of processor cores to use for multiprocessing. Recommend
+# rerun = False
+n_cores = 15 # Number of processor cores to use for multiprocessing. Recommend
 # n_cores = 1    # setting to 1 for debugging.
+parallelization_level = 'inner'     # Sets the level at which to do
+                                    # multiprocessing. If 'inner' then the level is
+                                    # in the inner loop over dichotomies. If
+                                    # 'outer' then the level is in the outer loop
+# parallelization_level = 'outer'   # over n_inputs and n_channels.
 
-n_dichotomies = 100 # Number of random dichotomies to test
+
+# n_dichotomies = 100 # Number of random dichotomies to test
+n_dichotomies = 50 # Number of random dichotomies to test
 n_inputs = [40] # Number of input samples to test
+# n_inputs = [16] # Number of input samples to test
 max_epochs = 500 # Maximum number of epochs if training with SGD
 # max_epochs_no_imp = 100 # Not implemented. Training will stop after
                           # this number of epochs without improvement
 # improve_tol = 1e-3 # Not implemented. The tolerance for improvement.
-batch_size = 512 # Batch size if training with SGD
-n_channels = list(range(10,60,2)) # Number of channels to test in output layer.
+batch_size = 256 # Batch size if training with SGD
+n_channels = list(range(10,50,2)) # Number of channels to test in output layer.
+# n_channels = [16] # Number of channels to test in output layer.
 # n_channels = list(range(5,17, 2)) # Number of channels to test in output layer.
-# n_channels = list(range(10,60,2)) # Number of channels to test in output layer.
 img_size_x = 20 # Size of image x dimension.
 img_size_y = 1 # Size of image y dimension.
-# net_style = 'conv' # Not fully implemented. Efficientnet layers.
+# img_size_x = 224 # Size of image x dimension.
+# img_size_y = 224 # Size of image y dimension.
+# net_style = 'conv' # Efficientnet layers.
 # net_style = 'grid' # Not fully implemented. Grid cell CNN.
-net_style = 'rand_conv' # Random convolutional layer.
-# net_style = 'randpoints' # Random points. Used to make sure linear
+# net_style = 'rand_conv' # Random convolutional layer.
+net_style = 'randpoints' # Random points. Used to make sure linear
                            # classifier is working alright.
 layer_idx = 0 # Index for layer to get from conv net. Currently only
               # implemented for net_style = 'conv'.
@@ -49,6 +59,8 @@ layer_idx = 0 # Index for layer to get from conv net. Currently only
 dataset_name = 'gaussianrandom' # Use Gaussian random inputs.
 # shift_style = '1d' # Take input 1d shifts (shift in only x dimension).
 shift_style = '2d' # Use input shifts in both x and y dimensions
+shift_x = 2 # Number of pixels by which to shift in the x direction
+shift_y = 2 # Number of pixels by which to shift in the y direction
 # pool = True # Whether or not to average (pool) the representation over the
 pool = False  # group before fitting the linear classifier.
 fit_intercept = True # Whether or not to fit the intercept in the linear
@@ -98,10 +110,26 @@ class ShiftDataset2D(torch.utils.data.Dataset):
     """Takes in a normal dataset of images and produces a dataset that
     samples from 2d shifts of this dataset, keeping the label for each
     shifted version of an image the same."""
-    def __init__(self, core_dataset):
+    def __init__(self, core_dataset, shift_x=1, shift_y=1):
+        """
+        Parameters
+        ----------
+        core_dataset : torch.utils.data.Dataset
+            The image dataset that we are computing shifts of.
+        shift_x : int
+            The number of pixels by which the image is shifted in the x
+            direction.
+        shift_y : int
+            The number of pixels by which the image is shifted in the y
+            direction.
+        """
         super().__init__()
         self.core_dataset = core_dataset
         self.sx, self.sy = self.core_dataset[0][0].shape[1:]
+        self.shift_x = shift_x
+        self.shift_y = shift_y
+        self.sx = self.sx // self.shift_x
+        self.sy = self.sy // self.shift_y
         # self.targets = torch.tile(torch.tensor(self.core_dataset.targets),
                                   # (self.sx*self.sy,))  # Too large for memory
         # self.G = itertools.product((range(self.xs), range(sy))) # Lazy approach
@@ -113,8 +141,8 @@ class ShiftDataset2D(torch.utils.data.Dataset):
         g_idx = idx % (self.sx*self.sy)
         idx_core = idx // (self.sx*self.sy)
         # g = self.G[g_idx] # Lazy approach
-        gx = g_idx // self.sy 
-        gy = g_idx % self.sy
+        gx = self.shift_x * (g_idx // self.sy)
+        gy = self.shift_y * (g_idx % self.sy)
         img, label = self.core_dataset[idx_core]
         return get_shifted_img(img, gx, gy), label, idx_core
 
@@ -122,10 +150,12 @@ class ShiftDataset1D(torch.utils.data.Dataset):
     """Takes in a normal dataset of images and produces a dataset that
     samples from 1d shifts of this dataset, keeping the label for each
     shifted version of an image the same."""
-    def __init__(self, core_dataset):
+    def __init__(self, core_dataset, shift_y=1):
         super().__init__()
         self.core_dataset = core_dataset
+        self.shift_y = shift_y
         self.sy = self.core_dataset[0][0].shape[-1]
+        self.sy = self.sy // self.shift_y
         # self.targets = torch.tile(torch.tensor(self.core_dataset.targets),
                                   # (self.sy,))
 
@@ -135,8 +165,7 @@ class ShiftDataset1D(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         g_idx = idx % self.sy
         idx_core = idx // self.sy
-        idx_core = idx // self.sy
-        gy = g_idx % self.sy
+        gy = self.shift_y * (g_idx % self.sy)
         img, label = self.core_dataset[idx_core]
         return get_shifted_img(img, 0, gy), label, idx_core
 
@@ -251,18 +280,16 @@ def get_capacity(n_channels, n_inputs):
         raise AttributeError('dataset_name option not recognized')
 
     if shift_style == '1d':
-        dataset = ShiftDataset1D(core_dataset)
+        dataset = ShiftDataset1D(core_dataset, shift_y)
     elif shift_style == '2d':
-        dataset = ShiftDataset2D(core_dataset)
-    elif shift_style == '2d_centroid':
-        dataset = ShiftCentroid2D(core_dataset)
+        dataset = ShiftDataset2D(core_dataset, shift_x, shift_y)
     else:
         raise AttributeError('Unrecognized option for shift_style.')
-    if n_cores > 1:
+    if n_cores > 1 and parallelization_level == 'inner':
         num_workers = 0
     else:
-        # num_workers = 3
-        num_workers = 0
+        num_workers = 4
+        # num_workers = 0
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
                                              num_workers=num_workers,
                                              shuffle=True)
@@ -354,7 +381,7 @@ def get_capacity(n_channels, n_inputs):
                     break
             return curr_avg_acc
         elif train_style == 'whole':
-            # print('Training standard SVM.')
+            print('Training standard SVM.')
             if pool:
                 ds = dataloader.dataset.core_dataset
                 n = len(ds)
@@ -402,6 +429,7 @@ def get_capacity(n_channels, n_inputs):
 
             fitter = svm.LinearSVC(tol=1e-12, max_iter=40000, C=30.,
                                   fit_intercept=fit_intercept)
+            # fitter = svm.LinearSVC(C=20., fit_intercept=fit_intercept)
             ## Debug code for checking rank of data 
             # Xmc = X - np.mean(X, axis=0)
             # C = X.T @ Xmc
@@ -415,11 +443,11 @@ def get_capacity(n_channels, n_inputs):
             return acc
 
 
-    if n_cores == 1:
-        class_acc_dichs = [dich_loop() for k1 in range(n_dichotomies)]
-    else:
+    if n_cores > 1 and parallelization_level == 'inner':
         class_acc_dichs = Parallel(n_jobs=n_cores)(
             delayed(dich_loop)() for k1 in range(n_dichotomies))
+    else:
+        class_acc_dichs = [dich_loop() for k1 in range(n_dichotomies)]
 
     capacity = (1.0*(torch.tensor(class_acc_dichs) == 1.0)).mean().item()
     if fit_intercept:
@@ -439,28 +467,30 @@ def get_capacity(n_channels, n_inputs):
 ## Run script by calling get_capacity 
 if __name__ == '__main__':
     param_list = list(itertools.product(n_channels, n_inputs))
-    results_table = pd.DataFrame()
-    # if run_mode == 'serial':
     torch.manual_seed(seed)
-    for n_input in n_inputs:
-        for n_channel in n_channels:
-            if fit_intercept:
-                alpha = n_input / (n_channel + 1)
-            else:
-                alpha = n_input / n_channel
-            capacity = get_capacity(n_channel, n_input)
-            d = {'n_channel': n_channel, 'n_input': n_input, 'alpha': alpha,
-                 'capacity': capacity}
+    results_table = pd.DataFrame()
+    if n_cores > 1 and parallelization_level == 'outer':
+        capacities = Parallel(n_jobs=n_cores)(
+            delayed(get_capacity)(*p) for p in param_list)
+        for k1, (n_input, n_channel) in enumerate(param_list):
+            capacity = capacities[k1]
+            d = {'n_channel': n_channel, 'n_input': n_input, 'alpha': n_input /
+                 n_channel, 'capacity': capacity}
             results_table = results_table.append(d, ignore_index=True)
+    else:
+        for n_input in n_inputs:
+            for n_channel in n_channels:
+                if fit_intercept:
+                    alpha = n_input / (n_channel + 1)
+                else:
+                    alpha = n_input / n_channel
+                capacity = get_capacity(n_channel, n_input)
+                d = {'n_channel': n_channel, 'n_input': n_input, 'alpha': alpha,
+                     'capacity': capacity}
+                results_table = results_table.append(d, ignore_index=True)
     # elif run_mode == 'parallel':
-        # capacities = Parallel(n_jobs=4)(
-            # delayed(get_capacity)(*p) for p in param_list)
-        # for k1, (n_input, n_channel) in enumerate(param_list):
-            # capacity = capacities[k1]
-            # d = {'n_channel': n_channel, 'n_input': n_input, 'alpha': n_input /
-                 # n_channel, 'capacity': capacity}
-            # results_table = results_table.append(d, ignore_index=True)
 
+    os.makedirs('figs', exist_ok=True)
     results_table.to_pickle('figs/most_recent.pkl')
     alpha_table = results_table.drop(columns=['n_channel', 'n_input'])
     fig, ax = plt.subplots()
