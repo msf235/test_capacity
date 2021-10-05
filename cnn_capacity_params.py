f2ae91d21d272e5543863e11e580f151222ebc45
Matthew Farrell <msfarrell@seas.harvard.edu>
2021-10-05 11:43:01 -0400

    further cleaned things up.

--- cnn_capacity_params.py
+++ cnn_capacity_params.py
@@ -1,6 +1,7 @@
 import torch
 import copy
 
+## Convenience functions
 def alphas_to_channels(alphas, n_inputs, fit_intercept):
     n_channels_temp = torch.round(n_inputs/alphas).int()
     n_channels_temp -= fit_intercept 
@@ -26,41 +27,30 @@ def exps_channels_and_layers(param_base, n_channels, layers=None):
             
     return exps
 
+
 param_sets = {}
 
 ## One-dimensional random CNN layer.
 random_1d_conv = dict(
     n_dichotomies = 100, # Number of random dichotomies to test
     n_inputs = 40, # Number of input samples to test
-    # n_inputs = 30, # Number of input samples to test
     max_epochs = 4000, # Maximum number of epochs.
-    # max_epochs_no_imp = 200,
-    # improve_tol = 1e-10
     batch_size = None, # Batch size if training with SGD
     img_size_x = 40, # Size of image x dimension.
     img_size_y = 1, # Size of image y dimension.
     net_style = 'rand_conv', # Random convolutional layer.
-    # layer_idx = 0,
-    # layer_idx = 1,
-    # layer_idx = 2,
     dataset_name = 'gaussianrandom', # Use Gaussian random inputs.
-    # img_channels = 100,
     img_channels = 3,
-    # perceptron_style = 'efficient',
     shift_style = '2d', # Use input shifts in both x and y dimensions
     shift_x = 1, # Number of pixels by which to shift in the x direction
     shift_y = 1, # Number of pixels by which to shift in the y direction
     pool_over_group = False, # group before fitting the linear classifier.
     pool='max', 
-    # pool=None,
-    # pool_x = 2,
-    # pool_y = 1,
     fit_intercept = False,
     center_response = False,
 )
 alphas = torch.linspace(0.5, 3.0, 15)
 layer_idx = [1, 2]
-# alphas = torch.linspace(1.5, 2.0, 2)
 n_channels = alphas_to_channels(alphas, random_1d_conv['n_inputs'],
     int(random_1d_conv['fit_intercept']))
 param_sets.update(
@@ -76,6 +66,7 @@ n_channels = alphas_to_channels(
 param_sets.update(
 randpoint_exps = exps_channels_and_layers(randpoint, n_channels)
 )
+
 randpoint_efficient = randpoint.copy()
 randpoint_efficient.update(
     perceptron_style = 'efficient'
@@ -104,6 +95,7 @@ param_sets.update(
 random_2d_conv_exps = exps_channels_and_layers(
     random_2d_conv, n_channels, layers=layer_idx)
 )
+
 random_2d_conv_gpool = random_2d_conv.copy()
 random_2d_conv_gpool.update(
     pool_over_group=True,
@@ -122,13 +114,6 @@ random_2d_conv_efficient_exps = exps_channels_and_layers(
     random_2d_conv_efficient, n_channels, layers=layer_idx)
 )
 
-random_2d_conv_gpool = random_2d_conv.copy()
-random_2d_conv_gpool.update(
-    pool_over_group=True
-)
-random_2d_conv_gpool_exps = exps_channels_and_layers(
-    random_2d_conv_gpool, n_channels, layers=layer_idx)
-
 ## Random CNN layer with 2 pixel shifts
 random_2d_conv_shift2 = random_2d_conv.copy()
 random_2d_conv_shift2.update(
@@ -144,21 +129,15 @@ random_2d_conv_shift2_exps = exps_channels_and_layers(
     random_2d_conv_shift2, n_channels)
 )
 
-
 ## VGG11 on CIFAR10
 vgg11_cifar10 = dict(
     n_dichotomies = 100, # Number of random dichotomies to test
-    # n_dichotomies = 20, # Number of random dichotomies to test
-    # n_dichotomies = 2, # Number of random dichotomies to test
-    # n_inputs = 12, # Number of input samples to test
     n_inputs = 20, # Number of input samples to test
     max_epochs = 500, # Maximum number of epochs.
     batch_size = None, # Batch size if training with SGD.
     img_size_x = 32, # Size of image x dimension.
     img_size_y = 32, # Size of image y dimension.
     net_style = 'vgg11',
-    # net_style = 'vgg11_circular',
-    # perceptron_style = 'efficient',
     dataset_name = 'cifar10', # Use imagenet inputs.
     shift_style = '2d', # Use input shifts in both x and y dimensions
     shift_x = 1, # Number of pixels by which to shift in the x direction
@@ -169,9 +148,7 @@ vgg11_cifar10 = dict(
     center_response = False,  # response 
 )
 alphas = torch.linspace(0.5, 3.0, 15)
-# alphas = torch.linspace(1.8, 2.2, 1)
 layer_idx = [2, 3, 6]
-# layer_idx = [3]
 n_channels = alphas_to_channels(
     alphas, vgg11_cifar10['n_inputs'],
     int(vgg11_cifar10['fit_intercept']))
@@ -197,10 +174,7 @@ vgg11_cifar10_efficient.update(
 )
 param_sets.update(
 vgg11_cifar10_efficient_exps = exps_channels_and_layers(
-    vgg11_cifar10_efficient, n_channels,
-    # layer_idx[-2:-1],
-    layer_idx,
-)
+    vgg11_cifar10_efficient, n_channels, layer_idx)
 )
 
 vgg11_cifar10_gpool = vgg11_cifar10_circular.copy()
@@ -212,82 +186,25 @@ vgg11_cifar10_gpool_exps = exps_channels_and_layers(
     vgg11_cifar10_gpool, n_channels, layer_idx)
 )
 
-vgg11_cifar10_gpool_lay2 = vgg11_cifar10_gpool.copy()
-vgg11_cifar10_gpool_lay2.update(
-    layer_idx=2,
-    n_inputs=20,
-)
-n_channels = alphas_to_channels(
-    alphas, vgg11_cifar10_gpool_lay2['n_inputs'],
-    int(vgg11_cifar10['fit_intercept']))
-vgg11_cifar10_gpool_lay2_exps = exps_channels_and_layers(
-    vgg11_cifar10_gpool_lay2, n_channels)
-
-vgg11_cifar10_gpool_lay3 = vgg11_cifar10_gpool.copy()
-vgg11_cifar10_gpool_lay3.update(
-    layer_idx=3,
-    n_inputs=20,
-)
-n_channels = alphas_to_channels(
-    alphas, vgg11_cifar10_gpool_lay3['n_inputs'],
-    int(vgg11_cifar10['fit_intercept']))
-vgg11_cifar10_gpool_lay3_exps = exps_channels_and_layers(
-    vgg11_cifar10_gpool_lay3, n_channels)
-
-vgg11_cifar10_gpool_lay6 = vgg11_cifar10_gpool.copy()
-vgg11_cifar10_gpool_lay6.update(
-    layer_idx=6,
-    n_inputs=20,
-)
-vgg11_cifar10_gpool_lay3_exps = exps_channels_and_layers(
-    vgg11_cifar10_gpool_lay3, n_channels)
-# vgg11_cifar10_gpool_exps = exps_channels_and_layers(
-    # vgg11_cifar10_gpool, n_channels, layer_idx[-2:-1])
-
 
 ## Grid cell net
 grid_2d_conv = dict(
     n_dichotomies = 100, # Number of random dichotomies to test
-    # n_dichotomies = 30, # Number of random dichotomies to test
-    # n_dichotomies = 5, # Number of random dichotomies to test
     n_inputs = 16, # Number of input samples to test
-    # n_inputs = 3, # Number of input samples to test
     max_epochs = 500, # Maximum number of epochs.
     batch_size=None,
-    # max_epochs_no_imp = 100, # Not implemented. Training will stop after
-                               # this number of epochs without improvement
-    # improve_tol = 1e-3, # Not implemented. The tolerance for improvement.
     img_size_x = 40, # Size of image x dimension.
     img_size_y = 40, # Size of image y dimension.
-    # img_size_x = 224, # Size of image x dimension.
-    # img_size_y = 224, # Size of image y dimension.
-    # net_style = 'effnet', # Efficientnet layers.
     net_style = 'grid', # Not fully implemented. Grid cell CNN.
-    # net_style = 'rand_conv', # Random convolutional layer.
-    # net_style = 'randpoints', # Random points. Used to make sure linear
-                                # classifier is working alright.
     layer_idx=0,
-    # dataset_name = 'imagenet', # Use imagenet inputs.
     dataset_name = 'gaussianrandom', # Use Gaussian random inputs.
-    # perceptron_style = 'efficient',
-    # shift_style = '1d', # Take input 1d shifts (shift in only x dimension).
     shift_style = '2d', # Use input shifts in both x and y dimensions
     shift_x = 1, # Number of pixels by which to shift in the x direction
     shift_y = 1, # Number of pixels by which to shift in the y direction
-    # pool_over_group = True, # Whether or not to average (pool) the representation over the
     pool_over_group = False, # group before fitting the linear classifier.
-    pool='max', 
-    pool_x = 2,
-    pool_y = 2,
-    # fit_intercept = True, # Whether or not to fit the intercept in the linear
-                          # classifier
     fit_intercept = False,
-    # center_response = True, # Whether or not to mean center each representation
     center_response = False,  # response 
 )
-# alphas = torch.linspace(.5, 3.0, 15)
-# alphas = torch.linspace(.5, 2.5, 15)
-# alphas = torch.linspace(1, 2.3, 15)
 alphas = torch.linspace(.8, 3.5, 20)
 n_channels = alphas_to_channels(
     alphas, grid_2d_conv['n_inputs'],
@@ -296,69 +213,4 @@ n_channels = [int(x/2) for x in n_channels]
 param_sets.update(
 grid_2d_conv_exps=exps_channels_and_layers(grid_2d_conv, n_channels)
 )
-
-
-######### Not Used #############################
-# AlexNet on imagenet
-alexnet_imagenet = dict(
-n_dichotomies = 100, # Number of random dichotomies to test
-n_inputs = [12], # Number of input samples to test
-alphas = torch.linspace(1.0, 3.0, 10),
-max_epochs = 500, # Maximum number of epochs.
-batch_size = 512, # Batch size if training with SGD.
-img_size_x = 224, # Size of image x dimension.
-img_size_y = 224, # Size of image y dimension.
-# img_size_x = 64, # Size of image x dimension.
-# img_size_y = 64, # Size of image y dimension.
-net_style = 'alexnet', # AlexNet layers.
-layer_idx = [1], # Index for layer to get from conv net.
-dataset_name = 'imagenet', # Use imagenet inputs.
-shift_style = '2d', # Use input shifts in both x and y dimensions
-shift_x = 14, # Number of pixels by which to shift in the x direction
-shift_y = 14, # Number of pixels by which to shift in the y direction
-# pool_over_group = True, # Whether or not to average (pool) the representation over the
-pool_over_group = False, # group before fitting the linear classifier.
-pool=None,
-fit_intercept = True, # Whether or not to fit the intercept in the linear
-                      # classifier
-# fit_intercept = False,
-# center_response = True, # Whether or not to mean center each representation
-center_response = False,  # response 
-)
-
-# Efficientnet on imagenet
-efficientnet_imagenet = dict(
-n_dichotomies = 100, # Number of random dichotomies to test
-n_inputs = 12, # Number of input samples to test
-max_epochs = 500, # Maximum number of epochs.
-batch_size = 512, # Batch size if training with SGD.
-img_size_x = 224, # Size of image x dimension.
-img_size_y = 224, # Size of image y dimension.
-# img_size_x = 64, # Size of image x dimension.
-# img_size_y = 64, # Size of image y dimension.
-net_style = 'effnet', # Efficientnet layers.
-layer_idx = 0, # Index for layer to get from conv net.
-dataset_name = 'imagenet', # Use imagenet inputs.
-shift_style = '2d', # Use input shifts in both x and y dimensions
-shift_x = 14, # Number of pixels by which to shift in the x direction
-shift_y = 14, # Number of pixels by which to shift in the y direction
-# pool_over_group = True, # Whether or not to average (pool) the representation over the
-pool_over_group = False, # group before fitting the linear classifier.
-pool=None,
-fit_intercept = True, # Whether or not to fit the intercept in the linear
-                      # classifier
-# fit_intercept = False,
-# center_response = True, # Whether or not to mean center each representation
-center_response = False,  # response 
-)
-alphas = torch.linspace(1.0, 3.0, 10)
-n_channels = alphas_to_channels(
-    alphas, efficientnet_imagenet['n_inputs'],
-    int(efficientnet_imagenet['fit_intercept']))
-efficientnet_imagenet_exps = []
-for n_channel in n_channels:
-    temp = efficientnet_imagenet.copy()
-    temp['n_channels'] = n_channel
-    efficientnet_imagenet_exps.append(temp)
-
 
--- plot_cnn_capacity.py
+++ plot_cnn_capacity.py
@@ -35,36 +35,12 @@ rerun = True # If True, rerun the simulation even if a matching simulation is
 
 # Number of processor cores to use for multiprocessing. Recommend setting to 1
 # for debugging
-n_cores = 2
+n_cores = 5
 seeds = [3, 4, 5]
 
 ## Collect parameter sets in a list of dictionaries so that simulations can be
 ## automatically saved and loaded based on the values in the dictionaries.
-# param_set_name = 'random_1d_conv_exps'
-# param_set_name = 'random_1d_conv_exps'
-# param_set_name = 'random_2d_conv_exps'
-# param_set_name = 'random_2d_conv_shift2_exps'
-# param_set_name = 'vgg11_cifar10_exps'
-# param_set_name = 'vgg11_cifar10_circular_exps'
-# param_set_name = 'vgg11_cifar10_efficient_exps'
 
-# param_set_names = ['randpoint_exps']
-# param_set_names = ['randpoint_efficient_exps']
-# param_set_names = ['vgg11_cifar10_efficient_exps', 'vgg11_cifar10_gpool_exps']
-# param_set_names = ['random_2d_conv_efficient_exps']
-# param_set_names = ['vgg11_cifar10_efficient_exps']
-# param_set_names = ['vgg11_cifar10_circular_exps']
-# param_set_names = ['vgg11_cifar10_gpool_exps']
-# param_set_names = ['random_2d_conv_exps', 'random_2d_conv_gpool_exps']
-param_set_names = ['random_2d_conv_exps']
-# param_set_names = ['random_2d_conv_gpool_exps']
-# param_set_names = ['vgg11_cifar10_exps']
-# param_set_names = ['grid_2d_conv_exps']
-print('Running {}'.format('  '.join(param_set_names)))
-
-param_set = []
-for name in param_set_names:
-    param_set += cp.param_sets[name]
 
 def cover_theorem(P, N):
     frac_dich = 0
@@ -73,8 +49,13 @@ def cover_theorem(P, N):
     frac_dich = 2**(1-1.0*P) * frac_dich
     return frac_dich
 
-## Run script by calling get_capacity 
-if __name__ == '__main__':
+def make_plot(param_set_names):
+    print('Running {}'.format('  '.join(param_set_names)))
+    
+    param_set = []
+    for name in param_set_names:
+        param_set += cp.param_sets[name]
+    
     plot_vars = ['n_channels', 'n_inputs', 'layer_idx']
 
     results_table = pd.DataFrame()
@@ -92,18 +73,13 @@ if __name__ == '__main__':
             else:
                 factor = 1
             offset = int(params['fit_intercept'])
-            pool_over_group = params['pool_over_group']
             alpha = n_input / (factor*n_channel + offset)
             cover_capacity = cover_theorem(n_input, n_channel)
-            pool_over_group = params['pool_over_group']
             d1 = {'seed': seed, 'alpha': alpha, 'n_inputs': n_input,
                   'n_channels': n_channel, 'n_channels_offset':
                   n_channel + offset, 'fit_intercept': params['fit_intercept'],
                   'layer': layer, 'net_style': net_style, 'capacity': capacity,
-                  'pool_over_group': pool_over_group
                  }
-            # for var in plot_vars:
-                # d1[var] = params[var]
             d1 = pd.DataFrame(d1, index=[0])
             results_table = results_table.append(d1, ignore_index=True)
 
@@ -114,19 +90,14 @@ if __name__ == '__main__':
         style = 'net_style'
     else:
         style = None
-    style = 'pool_over_group'
     os.makedirs('figs', exist_ok=True)
     alpha_table = results_table.drop(
         columns=['n_channels', 'n_inputs', 'n_channels_offset',
                  'fit_intercept'])
     fig, ax = plt.subplots(figsize=(5,4))
     g = sns.lineplot(ax=ax, x='alpha', y='capacity', data=alpha_table,
-                 hue='layer', style=style)
-    g.legend_.remove()
-    # sns.boxplot(ax=ax, x='alpha', y='capacity', data=alpha_table,
-                 # hue=style)
-    # sns.lineplot(ax=ax, x='alpha', y='capacity', data=alpha_table,
-                 # hue=style)
+                 hue='layer')
+    # g.legend_.remove()
     nmin = results_table['n_channels_offset'].min()
     nmax = results_table['n_channels_offset'].max()
     pmin = results_table['n_inputs'].min()
@@ -143,18 +114,33 @@ if __name__ == '__main__':
                 for p in range(pmin, pmax+1) if alphamin <= p/n <= alphamax}
     ax.plot(list(cover_cap.keys()), list(cover_cap.values()), linestyle='dotted',
            color='blue', label='theory')
-    # ax.plot(list(cover_cap_maxpool.keys()), list(cover_cap_maxpool.values()), linestyle='dotted',
-           # color='red', label='theory maxpool')
-    # ax.legend()
     P = param_set[0]['n_inputs']
     if param_set[0]['net_style'] == 'grid':
-       # ax.set_xlabel(r'$\alpha = $' + 'P' + r'/(2(\# channels))')
        ax.set_xlabel(r'$\alpha = P/N_0$')
     else:
-       # ax.set_xlabel(r'$\alpha = $' + 'P' + r'/(\# channels)')
        ax.set_xlabel(r'$\alpha = P/N_0$')
     ax.set_ylim([-.01, 1.01])
     figname = '__'.join(param_set_names)
     fig.savefig(f'figs/{figname}.pdf', bbox_inches='tight')
     results_table.to_pickle(f'figs/{figname}.pkl')
+
+
+## Run script by calling get_capacity 
+if __name__ == '__main__':
+
+    # Figure 2a
+    param_set_names = ['random_2d_conv_exps']
+    make_plot(param_set_names)
+
+    # Figure 2b
+    param_set_names = ['vgg11_cifar10_circular_exps']
+    make_plot(param_set_names)
+
+    # Figure 2c
+    param_set_names = ['grid_2d_conv_exps']
+    make_plot(param_set_names)
+
+    # Figure 3
+    param_set_names = ['vgg11_cifar10_exps']
+    make_plot(param_set_names)
 
